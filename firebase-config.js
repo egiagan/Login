b121936fbb1048b5886f036e6a27878df4f484f7
egiagan <30861646+egiagan@users.noreply.github.com>
2025-01-05 11:01:37 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDaP755gsLV7SGlY0oGwRMTmSoX06dJ5Qo",
-    authDomain: "multi-user-website.firebaseapp.com",
-    projectId: "multi-user-website",
-    storageBucket: "multi-user-website.appspot.com",
-    messagingSenderId: "912203168936",
-    appId: "1:912203168936:web:fc8d45a250a9d3f151d69d",
-    measurementId: "G-J86Y3N0Q0D"
+    apiKey: "AIzaSyDAlth1sxiP-S3r3fVDXhwadVVnEvpdO6s",
+    authDomain: "login-egi-agan.firebaseapp.com",
+    databaseURL: "https://login-egi-agan-default-rtdb.firebaseio.com",
+    projectId: "login-egi-agan",
+    storageBucket: "login-egi-agan.firebasestorage.app",
+    messagingSenderId: "395059466114",
+    appId: "1:395059466114:web:5c6b0621e9739df6b5c99b"
 };
 
 // Initialize Firebase
