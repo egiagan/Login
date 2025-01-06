207908856457ab3e190657effa49038a27e1ae5c
egiagan <30861646+egiagan@users.noreply.github.com>
2025-01-06 12:31:14 +0700

    Update and rename mainmain.js to index-saja.js

--- Igen/index-saja.js
+++ Igen/index-saja.js
@@ -101,3 +101,13 @@ function send_verification(){
 function myFunction_reload() {
     location.reload();
 }
+function switchView(view)
+{
+    $.get({
+        url:view,
+        cache:false,
+    })
+    .then(function(data){
+        $("#container").html(data);
+    });
+}
